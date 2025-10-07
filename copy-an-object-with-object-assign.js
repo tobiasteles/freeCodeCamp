9c41c52c7d57af52cb3172495d9058c5dc5d3fd0
Tobias Teles <tobias.teles@gmail.com>
2025-10-06 22:01:38 -0300

    redux complete

---
+++ copy-an-object-with-object-assign.js
@@ -0,0 +1,28 @@
+
+const defaultState = {
+  user: 'CamperBot',
+  status: 'offline',
+  friends: '732,982',
+  community: 'freeCodeCamp'
+};
+
+const immutableReducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case 'ONLINE':
+      return Object.assign({}, state, {status: 'online'}); // Don't mutate state here or the tests will fail
+      return
+    default:
+      return state;
+  }
+};
+
+const wakeUp = () => {
+  return {
+    type: 'ONLINE'
+  }
+};
+
+const store = Redux.createStore(immutableReducer);
+
+
+
---
+++ remove-an-item-from-an-array.js
@@ -0,0 +1,25 @@
+
+const immutableReducer = (state = [0,1,2,3,4,5], action) => {
+  switch(action.type) {
+    case 'REMOVE_ITEM':
+     return [...state.slice(0, action.index),
+     ...state.slice(action.index + 1)
+
+     ] // Don't mutate state here or the tests will fail
+      return
+    default:
+      return state;
+  }
+};
+
+const removeItem = (index) => {
+  return {
+    type: 'REMOVE_ITEM',
+    index
+  }
+}
+
+const store = Redux.createStore(immutableReducer);
+
+
+
